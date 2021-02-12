48705e7667df2d625e12c96d853aca0568665d5b
niklas <griessbaum@gmail.com>
2021-02-12 06:54:36 -0700

    init

---
+++ download.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import requests
+import time
+from eta import ETA
+
+
+class Granule:
+    
+    def __init__(self, url, folder):        
+        self.url = url
+        self.file_name = None
+        self.make_file_name()
+        self.session = requests.Session()
+        self.file_path = folder + '/' + self.file_name
+    
+    def make_file_name(self):
+        self.file_name = self.url.split('/')[-1]
+    
+    def already_downloaded(self):
+        if os.path.isfile(self.file_path):
+            print('file already downloaded. Skipping')
+            return True
+        else:
+            return False
+    
+    def download(self, user, pwd):
+        downloaded = self.already_downloaded()
+        auth = requests.auth.HTTPBasicAuth(user, pwd)
+        while not downloaded:                
+            try:
+                print(self.url)
+                ret = self.session.get(self.url, auth=auth)                   
+            except Exception as e:
+                print(e)
+                print('download failed, trying again')
+                time.sleep(1)     
+                continue
+            
+            if ret.status_code == 200:
+                downloaded = True
+                with open(self.file_path, 'wb') as out_file:
+                    out_file.write(ret.content)
+                out_file.write(ret.content)
+            elif ret.status_code == 401:
+                print('Got 401. Wrong user/pwd?')
+                break
+            else:
+                print(ret.status_code)
+                
+
+def download(folder, file_name, user, pwd):
+    urls = open(file_name).readlines()
+    eta = ETA(n_tot=len(urls))
+    for url in urls:
+        url = url.strip()
+        granule = Granule(url=url, folder=folder)
+        eta.display(step='Downloading {name}'.format(name=granule.file_name))
+        granule.download(user, pwd)
+        del urls[-1]
+        open(file_name, 'w').writelines(urls)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Downloads files from file list')
+    parser.add_argument('--file_list', metavar='csv', required=True, type=str, 
+                        help='CSV to read urls/fileIDs from')
+    parser.add_argument('--folder', metavar='folder', type=str, required=True,
+                        help='Destination folder', default='.')
+    parser.add_argument('--email', metavar='email', type=str, required=True,
+                        help='Email to be used as username and password on arthurhouhttps')
+    args = parser.parse_args()
+    
+    user= args.email
+    pwd = args.email
+
+    if args.file_list is None or args.folder is None:
+        print('Wrong usage')
+        print(parser.print_help())
+        quit()
+    
+
+    folder = os.path.expanduser(args.folder + '/')
+    download(folder, args.file_list, user, pwd)
---
+++ get_urls.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+import requests
+import bs4
+import re
+import datetime
+import time
+import argparse
+
+
+class ArthurhouFolder:
+    
+    def __init__(self, year, month, day, level):
+        self.session = requests.Session()
+        self.url_trunk = 'https://arthurhouhttps.pps.eosdis.nasa.gov'        
+        #self.login()
+        self.url = None
+        self.html = None
+        self.make_url(year, month, day, level)
+    
+    def login(self, user, pwd):
+        auth = requests.auth.HTTPBasicAuth(user, pwd)
+        self.session.get(self.url_trunk, auth=auth)
+    
+    def make_url(self, year, month, day, level):        
+        dataset = '/pub/gpmdata/'
+        date =  '{year}/{month:02d}/{day:02d}'
+        date = date.format(year=year, month=month, day=day)
+        level = '/{}/'.format(level)
+        self.url = self.url_trunk + dataset + date + level
+    
+    def download(self, user, pwd):
+        downloaded = False
+        auth = requests.auth.HTTPBasicAuth(user, pwd)
+        while not downloaded:                
+            try:
+                ret = self.session.get(self.url, auth=auth)                          
+            except Exception as e:
+                print(e)
+                print('download failed, trying again')
+                time.sleep(1)                
+            if ret.status_code == 200:
+                self.html = ret.text   
+                downloaded = True
+            elif ret.status_code == 401:
+                print('Got 401. Wrong user/pwd?')
+                break
+    
+    def extract_granule_links(self, product):
+        soup = bs4.BeautifulSoup(self.html, features="lxml")
+        term = '{product}*.*.HDF5'.format(product=product)
+        files = soup.find_all('a', href=re.compile(term))
+        links = []
+        for file in files:
+            links.append(self.url + file.get('href'))
+        return links
+    
+
+def get_urls(products, start, stop, user, level):
+    urls = []
+    start = datetime.datetime.strptime(start, '%Y-%m-%d').date()
+    stop = datetime.datetime.strptime(stop, '%Y-%m-%d').date()
+    step = datetime.timedelta(days=1)
+    iterator = start  
+    while iterator + step <= stop:
+        folder = ArthurhouFolder(iterator.year, iterator.month, iterator.day, level)
+        folder.download(user=user, pwd=user)
+        for product in products:
+            urls += folder.extract_granule_links(product)
+        iterator += step
+    return urls    
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get URLs from the ArthurHou server')
+    parser.add_argument('--products', metavar='products', nargs='+', 
+                        type=str, required=True, 
+                        help='xcal product (1C.GCOMW1.AMSR2, 1C.NPP.ATMS, 1C.NOAA20.ATMS, 1C.GPM.GMI, 1C.NOAA19.MHS, 1C.METOPB.MHS, 1C.MT1.SAPHIR, 1C.F16.SSMIS, 1C.F17.SSMIS, 1C.F18.SSMIS)')    
+    parser.add_argument('--level', metavar='level', required=True, type=str, 
+                        help='processing level (e.g. 1A, 1B, 1C)')
+    parser.add_argument('--user', metavar='user', required=True, type=str, 
+                        help='email address to be used as login and password', )    
+    parser.add_argument('--start', metavar='start', required=True, type=str, 
+                        help='start date (yyyy-mm-dd)')
+    parser.add_argument('--stop', metavar='stop', required=True, type=str, 
+                        help='stop date (yyyy-mm-dd)')
+    parser.add_argument('--out', metavar='out', required=False, type=str, 
+                        help='the csv filename to store the links in')
+    parser.set_defaults(out='out.csv')    
+    args = parser.parse_args()   
+    
+    urls = get_urls(args.products, args.start, args.stop, args.user, args.level)
+    with open(args.out, 'a') as files_log:        
+        files_log.writelines("\n,".join(urls))
+        files_log.write('\n')
